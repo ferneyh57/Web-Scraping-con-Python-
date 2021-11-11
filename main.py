921d2f1d7bbd3446ccaab8a1ce1ac584dd9c9a25
ferneyh57 <ferneyh57@gmail.com>
2021-11-11 00:19:26 -0500

    webdriver

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-a = 1
-b=2
+import csv
+from bs4 import BeautifulSoup
+
+
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+
+driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+driver.get("https://www.amazon.com/-/es/")
